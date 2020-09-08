d44d8960a67bbae7336fe5b4e4377fea0fa7cbd2
Axel <axel.svegerud@gmail.com>
2020-09-08 20:35:41 +0200

    First commit

---
+++ src/EDAF80/CelestialBody.cpp
@@ -0,0 +1,108 @@
+#include "CelestialBody.hpp"
+#include "glm/vec3.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+
+CelestialBody::CelestialBody(bonobo::mesh_data const& shape, GLuint const* program, GLuint diffuse_texture_id) {
+
+	_body.set_geometry(shape);
+	_body.set_program(program);
+	_body.add_texture("diffuse_texture", diffuse_texture_id, GL_TEXTURE_2D);
+}
+
+glm::mat4 CelestialBody::render(std::chrono::microseconds ellapsed_time, glm::mat4 const& view_projection, glm::mat4 const& parent_transform) {
+
+	std::chrono::duration<float> const ellapsed_time_s = ellapsed_time;
+	_spin_angle += ellapsed_time_s.count() * _spin_speed;
+
+	_orbit_angle += ellapsed_time_s.count() * _orbit_speed;
+
+
+	// ***SCALING:***
+
+	// Scaling matrix
+	glm::mat4 scale_matrix = glm::scale(glm::mat4(1.0f), _body_scale);
+
+	// ***SPINNING:***
+
+	// Spin matrix
+	glm::mat4 spin_matrix = glm::rotate(glm::mat4(1.0f), _spin_angle, glm::vec3(0, 1, 0));
+
+	// Tilt spin matrix
+	// "rotation by the inclination of the spin around the z-axis."
+	glm::mat4 tilt_spin_matrix = glm::rotate(glm::mat4(1.0f), _spin_inclination, glm::vec3(0, 0, 1));
+
+	// ***ORBITING:***
+
+
+	// Orbit matrix
+	// "rotate around the y-axis."
+	glm::mat4 orbit_matrix = glm::rotate(glm::mat4(1.0f), _orbit_angle, glm::vec3(0, 1, 0));
+
+	// Translation matrix
+	// "offset the planet by the radius of the orbit along the x-axis."
+	glm::mat4 translation_matrix = glm::mat4(1.0f);
+	translation_matrix[3][0] = _orbit_radius;
+
+	// Tilt orbit matrix
+	// "Tilt according to th orbit inclination angle => rotation around z-axis
+	glm::mat4 tilt_orbit_matrix = glm::rotate(glm::mat4(1.0f), _orbit_inclination, glm::vec3(0, 0, 1));
+
+	// Transform matrix
+	_transform_matrix = tilt_orbit_matrix * orbit_matrix * translation_matrix;
+
+	// Combination of the matrices
+	glm::mat4 matrix = _transform_matrix * tilt_spin_matrix * spin_matrix * scale_matrix;
+
+	// Combination of the matrices for rings.
+	// Rings should be rotated 90 deg around x-axis.
+	glm::mat4 matrix_rings =  _transform_matrix * tilt_spin_matrix * spin_matrix * glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1, 0, 0)) * scale_matrix;
+
+	_body.render(view_projection, matrix);
+
+	_ring_node.render(view_projection, matrix_rings);
+
+	return parent_transform * _transform_matrix;
+}
+
+void CelestialBody::set_scale(glm::vec3 const& scale) {
+	_body_scale = scale;
+}
+
+/*
+Problem when using struct!!!
+void CelestialBody::set_spin(SpinConfiguration const& configuration) {
+	_spin_configuration = configuration;
+}
+*/
+
+void CelestialBody::set_spin(float spin_inclination, float spin_speed) {
+	_spin_inclination = spin_inclination;
+	_spin_speed = spin_speed;
+}
+
+void CelestialBody::set_orbit(float orbit_radius, float orbit_inclination, float orbit_speed) {
+	_orbit_radius = orbit_radius;
+	_orbit_inclination = orbit_inclination;
+	_orbit_speed = orbit_speed;
+}
+
+void CelestialBody::set_ring(bonobo::mesh_data const& shape, GLuint const* program, GLuint diffust_texture_id, glm::vec2 const& scale) {
+	_ring_node.set_geometry(shape);
+	_ring_node.set_program(program);
+	_ring_node.add_texture("diffuse_texture", diffust_texture_id, GL_TEXTURE_2D);
+	_ring_scale = scale;
+}
+
+void CelestialBody::add_child(CelestialBody* child) {
+	_child_nodes.push_back( child );
+}
+
+std::vector<CelestialBody*> const& CelestialBody::get_children() const {
+	return _child_nodes;
+}
+
+glm::mat4 CelestialBody::get_transform() const {
+	return _transform_matrix;
+}
+
---
+++ src/EDAF80/CelestialBody.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "assignment1.hpp"
+#include "core/helpers.hpp"
+#include "../core/node.hpp"
+
+class CelestialBody {
+public:
+
+	// - shape contains information about the geometry used to model the celestial body.
+	// - program is the shader program used to render the celestial body.
+	// - diffuse_texture_id is the identifer of the difuse texture used.
+
+	CelestialBody(bonobo::mesh_data const& shape, GLuint const* program, GLuint diffuse_texture_id);
+
+	// - The returned matrix is the one used as the parent_transform for that body's children.
+	// - ellapsed_time is the amount of time (in microseconds) between two frames.
+	// - view_projection is the matrix for going from world-space to clip-space.
+	// - parent_transform transforms from the local-space of your parent, to world-space, and defaults to the identity matrix if unspecifed.
+
+	glm::mat4 render(std::chrono::microseconds ellapsed_time, glm::mat4 const& view_projection, glm::mat4 const& parent_transform = glm::mat4(1.0f));
+
+	void set_scale(glm::vec3 const& scale);
+
+	// Problem when using struct!!!
+	// void set_spin(SpinConfiguration const& configuration);
+
+	// void set_orbit(OrbitConfiguration const& configuration);
+
+	void set_spin(float spin_inclination, float spin_speed);
+
+	void set_orbit(float orbit_radius, float orbit_inclination, float orbit_speed);
+
+	void set_ring(bonobo::mesh_data const& shape, GLuint const* program, GLuint diffust_texture_id, glm::vec2 const& scale = glm::vec2(1.0f));
+
+	/* Will append child to a new private attribute of type
+	std::vector<CelestialBody*>, by using the attribute's
+	push_back(CelestialBody * item) method. */
+	void add_child(CelestialBody* child);
+
+	// "Will simply return the attribute you created in the previous step."
+	std::vector<CelestialBody*> const& get_children() const;
+
+	glm::mat4 get_transform() const;
+
+private:
+	// private node from the class Node.
+	Node _body;
+	Node _ring_node;
+
+	// Vectors:
+	glm::vec3 _body_scale = glm::vec3(1, 1, 1);
+	glm::vec2 _ring_scale = glm::vec2(1.0f);
+	std::vector<CelestialBody*> _child_nodes;
+
+	// Matrices:
+	glm::mat4 _transform_matrix;
+
+	// Floats:
+	float _spin_angle = 0.0f;
+	float _spin_inclination = 0.0f;
+	float _spin_speed = 0.0f;
+	float _orbit_angle = 0.0f;
+	float _orbit_inclination = 0.0f;
+	float _orbit_speed = 0.0f;
+	float _orbit_radius = 0.0f;
+
+};
